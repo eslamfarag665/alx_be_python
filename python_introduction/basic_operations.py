a403b483190deb758e123bc9438db2a0c4974c54
Eslam Farag <121220560+eslamfarag665@users.noreply.github.com>
2024-06-23 23:41:49 +0300

    Add files via upload

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+addit = number1+number2
+Subtr = number1-number2
+mult = number1*number2
+print("addition","of",number1,"and",number2,"is",addit)
+print("Subtraction","of",number1,"and",number2,"is",Subtr)
+print("Multiplication","of",number1,"and",number2,"is",mult)
