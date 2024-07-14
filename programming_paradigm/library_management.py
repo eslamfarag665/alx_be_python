0e2db694cfb2e6a61b03392acd9d506985f79d3f
eslamfarag665 <Eslamfarag665@gmail.com>
2024-07-14 17:23:32 +0300

    add new file

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,3 +1,5 @@
+# library_management.py
+
 class Book:
     """A class representing a book in the library."""
     
@@ -5,9 +7,24 @@ class Book:
         self.title = title
         self.author = author
         self.isbn = isbn
+        self.checked_out = False
 
     def __str__(self):
         return f"'{self.title}' by {self.author} (ISBN: {self.isbn})"
+
+    def check_out(self):
+        """Check out the book."""
+        if not self.checked_out:
+            self.checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        """Return the book."""
+        if self.checked_out:
+            self.checked_out = False
+            return True
+        return False
 
 class Library:
     """A class representing a library."""
