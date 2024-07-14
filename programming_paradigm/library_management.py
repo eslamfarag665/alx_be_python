413839df6057645b9eb35da1df4c77a6bda85d33
eslamfarag665 <Eslamfarag665@gmail.com>
2024-07-14 17:21:03 +0300

    Merge branch 'main' of https://github.com/eslamfarag665/alx_be_python

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,21 +1,39 @@
+class Book:
+    """A class representing a book in the library."""
+    
+    def __init__(self, title, author, isbn):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
 
-class SimpleCalculator:
-    """A simple calculator class that supports basic arithmetic operations."""
+    def __str__(self):
+        return f"'{self.title}' by {self.author} (ISBN: {self.isbn})"
 
-    def add(self, a, b):
-        """Return the addition of a and b."""
-        return a + b
+class Library:
+    """A class representing a library."""
 
-    def subtract(self, a, b):
-        """Return the subtraction of b from a."""
-        return a - b
+    def __init__(self):
+        self.books = []
 
-    def multiply(self, a, b):
-        """Return the multiplication of a and b."""
-        return a * b
+    def add_book(self, book):
+        """Add a book to the library."""
+        self.books.append(book)
 
-    def divide(self, a, b):
-        """Return the division of a by b. Returns None if b is zero."""
-        if b == 0:
-            return None
-        return a / b
+    def remove_book(self, isbn):
+        """Remove a book from the library by its ISBN."""
+        for book in self.books:
+            if book.isbn == isbn:
+                self.books.remove(book)
+                return True
+        return False
+
+    def find_book(self, isbn):
+        """Find a book in the library by its ISBN."""
+        for book in self.books:
+            if book.isbn == isbn:
+                return book
+        return None
+
+    def list_books(self):
+        """List all books in the library."""
+        return [str(book) for book in self.books]
