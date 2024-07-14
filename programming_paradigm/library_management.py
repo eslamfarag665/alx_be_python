a44aa36783aa3fda180f1aba2a135169ff7a016e
eslamfarag665 <Eslamfarag665@gmail.com>
2024-07-14 17:24:59 +0300

    add new file

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -30,27 +30,41 @@ class Library:
     """A class representing a library."""
 
     def __init__(self):
-        self.books = []
+        self._books = []
 
     def add_book(self, book):
         """Add a book to the library."""
-        self.books.append(book)
+        self._books.append(book)
 
     def remove_book(self, isbn):
         """Remove a book from the library by its ISBN."""
-        for book in self.books:
+        for book in self._books:
             if book.isbn == isbn:
-                self.books.remove(book)
+                self._books.remove(book)
                 return True
         return False
 
     def find_book(self, isbn):
         """Find a book in the library by its ISBN."""
-        for book in self.books:
+        for book in self._books:
             if book.isbn == isbn:
                 return book
         return None
 
     def list_books(self):
         """List all books in the library."""
-        return [str(book) for book in self.books]
+        return [str(book) for book in self._books]
+
+    def check_out_book(self, isbn):
+        """Check out a book from the library by its ISBN."""
+        book = self.find_book(isbn)
+        if book and book.check_out():
+            return True
+        return False
+
+    def return_book(self, isbn):
+        """Return a book to the library by its ISBN."""
+        book = self.find_book(isbn)
+        if book and book.return_book():
+            return True
+        return False
